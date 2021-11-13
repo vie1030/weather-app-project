fd1a822b974aa4f88ea3e5a3ed2f825bde92b029
vie1030 <91474368+vie1030@users.noreply.github.com>
2021-11-13 15:50:07 +0100

    update the weather icon

--- src/weather.js
+++ src/weather.js
@@ -23,24 +23,29 @@ function formatDate(timestamp) {
 }
 
 function showTemperature(response) {
-  let dateElement = document.querySelector("#date");
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
-
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].description;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let highTemperatureElement = document.querySelector("#highTemperature");
   highTemperatureElement.innerHTML = Math.round(response.data.main.temp_max);
   let lowTemperatureElement = document.querySelector("#lowTemperature");
   lowTemperatureElement.innerHTML = Math.round(response.data.main.temp_min);
   let feelElement = document.querySelector("#feel");
   feelElement.innerHTML = Math.round(response.data.main.feels_like);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-
+let city = "Tokyo";
 let apiKey = "867dcf6bb3756e0001b67ad06b6f1ecd";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemperature);
