2435cd9c80c31dbe45bffe934b8f69701eb49ad8
vie1030 <91474368+vie1030@users.noreply.github.com>
2021-11-18 18:17:30 +0100

    added the forecast in JS

--- src/weather.js
+++ src/weather.js
@@ -22,6 +22,34 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function showForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+           <div class="col-2">
+                <div class="forecast-date">${day}</div>
+                <img
+                  src="https://openweathermap.org/img/wn/50d@2x.png"
+                  alt=""
+                  width="40"
+                />
+                <div class="forecast-temperature">
+                  <span class="forecast-temp-max"> 7° </span>
+                  <span class="forecast-temp-min"> 5° </span>
+                </div>
+          </div>
+    `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function showTemperature(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
@@ -87,3 +115,4 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 search("Tokyo");
+showForecast();
